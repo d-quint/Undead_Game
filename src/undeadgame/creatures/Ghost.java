77bef891c569bac2b708153ab69c8f679195f58a
d-quint <112879320+d-quint@users.noreply.github.com>
2023-10-04 14:03:32 +0800

    Refactor Ghost class to implement Commandable interface
    god

--- src/undeadgame/creatures/Ghost.java
+++ src/undeadgame/creatures/Ghost.java
@@ -1,13 +1,26 @@
 package undeadgame.creatures;
 
-// // Ghost: Ghost are like virtual version of an undead. It inherits all the characteristics that the undead has. Ghost may attack other undead. Its attack damage is only 20% of its HP. Ghost only receives 10% of the damage being done to it. If ghost HP is reduced to 0, it will be perished. Ghost initial HP would be the half of the initial HP of the undead. Ghost can haunt which increases its HP by the 10% of the undead being haunt.
+/**
+ * This class is used to represent a Ghost. Ghosts are like virtual version of an
+ * undead. It inherits all the characteristics that the undead has. Ghost may
+ * attack other undead. Its attack damage is only 20% of its HP. Ghost only
+ * receives 10% of the damage being done to it. If ghost HP is reduced to 0, it
+ * will be perished. Ghost initial HP would be the half of the initial HP of the
+ * undead. Ghost can haunt which increases its HP by the 10% of the undead being
+ * haunt.
+ */
+public class Ghost extends Undead implements Commandable {
+  public static final int MAX_HP = 50; // Ghost's max HP is 50.
 
-public class Ghost extends Undead {
+  private String[] commands = {"Normal Attack", "Haunt"}; // Ghost's commands.
+  private String[] commandDesc = {
+    "Attack your enemy with your ghostly powers! (Damage: 20% of your HP)",
+    "Heal yourself by 10% of your target's HP by haunting them!"
+  }; // Ghost's command descriptions.
+
   // Constructor:
   public Ghost(String name) {
     super(name + " (Ghost)", 50);
-    super.setType(Type.GHOST);
-    super.setMaxHP(50);
   }
 
   @Override
@@ -15,28 +28,43 @@ public class Ghost extends Undead {
     super.setName(name + " (Ghost)");
   }
 
+  
+  // Custom methods (Overridden methods from Commandable interface):
+
   @Override
-  public int attack(Undead target) {
-    return target.receiveDamage((int)(super.getHP() * 0.2)); // Ghost's attack damage is 20% of its HP.
+  public String getHPString() {
+    return super.getHp() + "/" + MAX_HP;
   }
 
   @Override
   public int receiveDamage(int damage) {
-    return super.receiveDamage((int) (damage * 0.1)); // Ghost only receives 10% of the damage being done to it.
+    damage = (int)(damage * 0.1); // Ghost only receives 10% of the damage being done to it.
+    super.setHp(super.getHp() - damage); // Reduce the ghost's HP by the damage received.
+
+    this.update();
+
+    return damage;
   }
 
   @Override
   public void update() {
-    if (super.getHP() > super.getMaxHP()) { // Limit the ghost's HP to its max HP.
-      super.setHP(super.getMaxHP());
+    if (super.getHp() > MAX_HP) { // Limit the ghost's HP to its max HP.
+      super.setHp(MAX_HP);
     }
 
-    super.update();
+    if (super.getHp() <= 0) { // If ghost HP is reduced to 0, it will be perished.
+      super.setHp(0);
+      super.isDead(true);
+    }
   }
 
-  // Custom methods:
+  @Override
+  public int normalAttack(Commandable target) {
+    return target.receiveDamage((int)(super.getHp() * 0.2)); // Ghost's attack damage is 20% of its HP.
+  }
 
   /**
+   * SKILL 1: HAUNT
    * This method is called when the ghost haunts another undead. The ghost will
    * receive 10% of the HP of the undead being haunted. The ghost's HP will be
    * increased by the amount of HP received.
@@ -44,8 +72,15 @@ public class Ghost extends Undead {
    * @param  target  The undead being haunted.
    * @return         The amount of HP received by the ghost.
    */
-  public int haunt(Undead target) {
-    int heal = (int) (target.getHP() * 0.1);
-    return -super.receiveDamage(-heal);
+  @Override
+  public int skill1(Undead target) {
+    int heal = (int) (target.getHp() * 0.1);
+    super.setHp(super.getHp() + heal);
+    return heal;
+  }
+
+  @Override
+  public int skill2(Commandable u) {
+    return -444; // Ghost has no skill 2.
   }
 }
--- src/undeadgame/creatures/Undead.java
+++ src/undeadgame/creatures/Undead.java
@@ -1,139 +1,189 @@
 package undeadgame.creatures;
 
-public class Undead {
-  public enum Type {
-    ZOMBIE,
-    VAMPIRE,
-    SKELETON,
-    GHOST,
-    NORMAL
-  }
-  
-  private Type undeadType;
+class Undead {
+	// attributes
+	private String name;
+	private int hp;
+	private boolean isDead;
 
-  // Attributes:
-  private String name;
-  private int hp = 100;
-  private boolean isDead;
-  private boolean canAttack;
+	// default constructor
+	public Undead() {
+		hp = 100;
+		name = "Undead";
+		isDead = false;
+	}
 
-  private int MAX_HP;
+	public Undead(String name, int hp) {
+		this.hp  = hp;
+		this.name = name;
+		isDead = false;
+	}
 
-  // Constructor:
-  public Undead() {
-    undeadType = Type.NORMAL;
-    
-    this.hp = 100;
-    this.name = "Undead - " + name;
-    this.isDead = false;
-    this.canAttack = true;
+	// setter & getter
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    this.MAX_HP = 100;
-  }
-  
-  public Undead(String name, int hp) {
-    undeadType = Type.NORMAL;
-    
-    this.hp = hp;
-    this.name = "Undead " + name;
-    this.isDead = false;
-    this.canAttack = true;
+	public String getName() {
+		return name;
+	}
 
-    this.MAX_HP = hp;
-  }
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
 
-  // Setters:
-  public void setType(Type type) {
-    this.undeadType = type;
-  }
+	public int getHp() {
+		return hp;
+	}
 
-  public void setName(String name) {
-    this.name = "Undead " + name;
-  }
+	public void isDead(boolean isDead) {
+		this.isDead = isDead;
+	}
 
-  public void setHP(int hp) {
-    this.hp = hp;
-  }
+	public boolean isDead() {
+		return isDead;
+	}
 
-  public void setMaxHP(int maxHP) {
-    this.MAX_HP = maxHP;
-  }
-
-  public void isDead(boolean isDead) {
-    this.isDead = isDead;
-  }
-
-  public void canAttack(boolean canAttack) {
-    this.canAttack = canAttack;
-  }
-
-  // Getters:
-  public Type getType() {
-    return undeadType;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public int getHP() {
-    return hp;
-  }
-
-  public int getMaxHP() {
-    return MAX_HP;
-  }
-
-  public boolean isDead() {
-    return isDead;
-  }
-
-  public boolean canAttack() {
-    return canAttack;
-  }
-
-  // Custom methods:
-
-  /**
-   * This method is called when the undead attacks another undead. 
-   * The undead, by default, will receive damage equal to its HP.
-   * 
-   * @param target
-   * @return
-   */
-  public int attack(Undead target) {
-    return target.receiveDamage(hp);
-  }
-
-  /**
-   * This method is called when the undead receives damage from another undead.
-   * The undead will receive damage equal to the damage parameter.
-   * 
-   * @param damage
-   * @return
-   */
-  public int receiveDamage(int damage) {
-    hp -= damage;
-    update(); // Check if the undead is dead or immobilized.
-    return damage;
-  }
-
-  /**
-   * This method is called after every attack. If the HP of the undead is 0 or
-   * less, it will die. Otherwise, it will not die.
-   * 
-   * Since not all undeads die at 0 HP, this method is overridden in the subclasses.
-   */
-  public void update() {
-    if (hp <= 0) {
-      hp = 0;
-      isDead = true;
-    } else {
-      isDead = false;
-    }
-  }
-
-  public String getHPstring() {
-    return getHP() + "/" + getMaxHP();
-  }
+	// custom method
+	public void attack(Undead u) {
+		u.setHp(u.getHp() - hp);
+	}
 }
+
+// public class Undead {
+//   public enum Type {
+//     ZOMBIE,
+//     VAMPIRE,
+//     SKELETON,
+//     GHOST,
+//     NORMAL
+//   }
+  
+//   private Type undeadType;
+
+//   // Attributes:
+//   private String name;
+//   private int hp = 100;
+//   private boolean isDead;
+//   private boolean canAttack;
+
+//   private int MAX_HP;
+
+//   // Constructor:
+//   public Undead() {
+//     undeadType = Type.NORMAL;
+    
+//     this.hp = 100;
+//     this.name = "Undead - " + name;
+//     this.isDead = false;
+//     this.canAttack = true;
+
+//     this.MAX_HP = 100;
+//   }
+  
+//   public Undead(String name, int hp) {
+//     undeadType = Type.NORMAL;
+    
+//     this.hp = hp;
+//     this.name = "Undead " + name;
+//     this.isDead = false;
+//     this.canAttack = true;
+
+//     this.MAX_HP = hp;
+//   }
+
+//   // Setters:
+//   public void setType(Type type) {
+//     this.undeadType = type;
+//   }
+
+//   public void setName(String name) {
+//     this.name = "Undead " + name;
+//   }
+
+//   public void setHP(int hp) {
+//     this.hp = hp;
+//   }
+
+//   public void setMaxHP(int maxHP) {
+//     this.MAX_HP = maxHP;
+//   }
+
+//   public void isDead(boolean isDead) {
+//     this.isDead = isDead;
+//   }
+
+//   public void canAttack(boolean canAttack) {
+//     this.canAttack = canAttack;
+//   }
+
+//   // Getters:
+//   public Type getType() {
+//     return undeadType;
+//   }
+
+//   public String getName() {
+//     return name;
+//   }
+
+//   public int getHP() {
+//     return hp;
+//   }
+
+//   public int getMaxHP() {
+//     return MAX_HP;
+//   }
+
+//   public boolean isDead() {
+//     return isDead;
+//   }
+
+//   public boolean canAttack() {
+//     return canAttack;
+//   }
+
+//   // Custom methods:
+
+//   /**
+//    * This method is called when the undead attacks another undead. 
+//    * The undead, by default, will receive damage equal to its HP.
+//    * 
+//    * @param target
+//    * @return
+//    */
+//   public int attack(Undead target) {
+//     return target.receiveDamage(hp);
+//   }
+
+//   /**
+//    * This method is called when the undead receives damage from another undead.
+//    * The undead will receive damage equal to the damage parameter.
+//    * 
+//    * @param damage
+//    * @return
+//    */
+//   public int receiveDamage(int damage) {
+//     hp -= damage;
+//     update(); // Check if the undead is dead or immobilized.
+//     return damage;
+//   }
+
+//   /**
+//    * This method is called after every attack. If the HP of the undead is 0 or
+//    * less, it will die. Otherwise, it will not die.
+//    * 
+//    * Since not all undeads die at 0 HP, this method is overridden in the subclasses.
+//    */
+//   public void update() {
+//     if (hp <= 0) {
+//       hp = 0;
+//       isDead = true;
+//     } else {
+//       isDead = false;
+//     }
+//   }
+
+//   public String getHPstring() {
+//     return getHP() + "/" + getMaxHP();
+//   }
+// }
